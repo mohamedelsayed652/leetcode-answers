57be13e47d470207d28905ecd645043a97df32d2
Mohamed El Sayed <102246128+mohamedelsayed652@users.noreply.github.com>
2025-10-05 13:43:15 -0400

    Time: 3 ms (25.94%) | Memory: 18 MB (8.25%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(strs[0])):#iterate through the first word
             char = strs[0][i] #char is equal to i letter of first word
             for j in range(1,len(strs)): #iterate through the rest of the list
-                if i >= len(strs[j]) or strs[j][i] != char: #if mismatch is found
+                if i >= len(strs[j]) or strs[j][i] != char: #if mismatch is found or current word is shorter than the first word
                     return LCP #return the Longest common prefix
             LCP = LCP + char #else add current character to the Longest common prefix
         return LCP
