818398d230875b933973defd49afd2c2f1d70501
Mohamed El Sayed <102246128+mohamedelsayed652@users.noreply.github.com>
2025-10-05 13:42:48 -0400

    Time: 3 ms (25.94%) | Memory: 17.8 MB (78.44%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,15 +1,10 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        
-        prefix = ""
-
-        for i in range(len(strs[0])): # iterate the first word's letters
-            char = strs[0][i]                # pick current character
-            for word in strs[1:]:            # compare with all other words
-                if i >= len(word) or word[i] != char:
-                    return prefix             # mismatch
-            prefix += char                    
-        
-        return prefix
+        LCP = ""
+        for i in range(len(strs[0])):#iterate through the first word
+            char = strs[0][i] #char is equal to i letter of first word
+            for j in range(1,len(strs)): #iterate through the rest of the list
+                if i >= len(strs[j]) or strs[j][i] != char: #if mismatch is found
+                    return LCP #return the Longest common prefix
+            LCP = LCP + char #else add current character to the Longest common prefix
+        return LCP
